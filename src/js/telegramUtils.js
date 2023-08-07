1724d6574d98d1f87a929714c7dcd447ccb10d52
Fresh <solovev-matveyka@inbox.ru>
2023-08-07 17:29:16 +0700

    FIXED: fetchData takes date from fields

--- src/js/telegramUtils.js
+++ src/js/telegramUtils.js
@@ -17,13 +17,20 @@ function fetchData() {
     let event_name = document.getElementById('event-name').value;
     let start_time_raw = document.getElementById('start-time').value;
     let end_time_raw = document.getElementById('end-time').value;
-    let date_raw = document.getElementById('date').value;
+    let date_start_raw = document.getElementById('date-start').value;
+    let date_end_raw;
+    if (document.getElementById('date-end') === null) {
+        date_end_raw = date_start_raw;
+    }
+    else {
+        date_end_raw = document.getElementById('date-end').value;
+    }
     let description = document.getElementById('event-description').value;
     let latitude = 0;
     let longitude = 0;
 
-    let start_time = toDate(date_raw, start_time_raw).toString();
-    let end_time = toDate(date_raw, end_time_raw).toString();
+    let start_time = toDate(date_start_raw, start_time_raw).toString();
+    let end_time = toDate(date_end_raw, end_time_raw).toString();
 
     let data = {
         web_app_name: "create_event",
