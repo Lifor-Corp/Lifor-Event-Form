fdde47f09a8ab4754dd31c95dd4fe228fb8c6eee
Fresh <solovev-matveyka@inbox.ru>
2023-08-07 16:48:01 +0700

    ADDED: user can add event end date

---
+++ src/js/fieldsProcessing.js
@@ -0,0 +1,16 @@
+function addDateField() {
+    const block = document.querySelector("#date-block");
+    const button = document.querySelector("#add-date");
+    button.remove();
+    block.querySelector(".date-block").classList.add("date-start");
+    block.querySelector(".date-start").classList.remove("date-block");
+    
+    const dateEnd = document.createElement("input");
+    dateEnd.type = "date";
+    dateEnd.id = "date-end";
+    dateEnd.name = "date-end";
+    dateEnd.classList.add("bg-slate-100", "rounded", "block", "w-full", "mt-1", "date-end");
+    block.appendChild(dateEnd);
+}
+
+document.querySelector("#add-date").addEventListener("click", addDateField);
